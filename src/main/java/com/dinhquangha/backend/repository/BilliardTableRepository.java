a7d996d69b8d80991611d0bcf15e219aa2b70aba
Quangha Ha <dquangha366@gmail.com>
2026-01-21 14:41:40 +0700

    fix: update axios baseURL for production

--- src/main/java/com/dinhquangha/backend/repository/BilliardTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dinhquangha.backend.repository;
-
-import com.dinhquangha.backend.model.BilliardTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BilliardTableRepository extends JpaRepository<BilliardTable, Long> {
-}
